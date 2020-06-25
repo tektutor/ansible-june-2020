8182a9cf5ca6df7b7f9aeca4e20861aeb32507cb
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-06-25 10:55:43 +0530

    Added Ansible Dynamic Inventory.

--- Day4/python/greet.py
+++ Day4/python/greet.py
@@ -4,13 +4,33 @@ class Greeting:
    
    def __init__(self):
        print ("Greeting constructor ...")
+       self.x = 0
+       self.y = 0
 
-   def sayHello(self):
-       print ( 'Hello Python !')
+   def setValues(self, value1, value2 ):
+       self.x = value1
+       self.y = value2
+
+   def printValues(self):
+       print ( 'X = ' + str ( self.x ) )
+       print ( 'Y = ' + str ( self.y ) )
+
+   def sayHello(self, msg):
+       print ( 'Hello ' + msg )
 
 
 obj1 = Greeting()
-obj1.sayHello()
+obj1.sayHello("Python !")
+print('#Obj1 - Value of X & Y before set Values ...')
+obj1.printValues()
+print('#Obj1 - Value of X & Y after set Values ...')
+obj1.setValues(10,20)
+obj1.printValues()
+
+obj2 = Greeting()
+obj2.sayHello("Ansible !")
+print('#Obj2 - Value of X & Y before set Values ...')
+obj2.printValues()
 
 
 
