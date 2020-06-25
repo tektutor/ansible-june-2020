ce7a56041ab65f6a0a29bc34f428b8dad9258584
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-06-25 10:37:53 +0530

    Added python lab exercises.

---
+++ Day4/python/greet.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+class Greeting:
+   
+   def __init__(self):
+       print ("Greeting constructor ...")
+
+   def sayHello(self):
+       print ( 'Hello Python !')
+
+
+obj1 = Greeting()
+obj1.sayHello()
+
+
+
+
+
+
+
+
---
+++ Day4/python/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+
+def sayHello():
+    print ( "Hello Python!" )
+
+sayHello()
